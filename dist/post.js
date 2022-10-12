67e3a512709fa19e4a09d43cc813cebb922fdbf2
Takeo Sawada <myc.monad@gmail.com>
2022-10-12 19:20:41 +0900

    wip

--- dist/post.js
+++ dist/post.js
@@ -1,4 +1,12 @@
 "use strict";
 exports.__esModule = true;
 var http = require("http");
-http.get("http://localhost:3055/close");
+http.get("http://localhost:3055/close", {}, (response) => {
+    var s = '';
+    response.on('data', (chunk) => {
+        str += chunk;
+    });
+    response.on('end', () => {
+        console.log(str);
+    })
+});
--- src/main.ts
+++ src/main.ts
@@ -1,52 +1,67 @@
 import * as http from 'http'
 import { parse } from 'url'
+import { spawn } from 'child_process'
 
 console.log("hello, world!");
 
-const server = http.createServer();
-let n_req: number = 0;
-let n_hit: number = 0;
-let n_miss: number = 0;
+if (!process.env.IS_BACKGROUND) {
+    console.log('spawning');
+    const child = spawn(process.execPath, process.argv.slice(1), {
+        detached: true,
+        stdio: 'inherit',
+        env: {...process.env, IS_BACKGROUND: '1'}
+    });
+    child.unref();
+} else {
+    main();
+}
 
-const baseUrl = process.env.ACTIONS_CACHE_URL + '/_apis/artifactcache/';
-const token = process.env.ACTIONS_RUNTIME_TOKEN;
+function main() {
+    const server = http.createServer();
+    let n_req: number = 0;
+    let n_hit: number = 0;
+    let n_miss: number = 0;
 
-server.on('request', (request, response) => {
-    const url = parse(request.url);
-    console.log(url.pathname);
-    if (url.pathname == '/close') {
-        server.close();
-        response.writeHead(200);
-        response.end();
-    } else if (url.pathname.startsWith('/cas/')) {
-        n_req += 1;
-        const hash = url.pathname.substring(5);
-        http.get(baseUrl + 'cache?key=cas-' + hash, {
-            headers: {
-                'Authorization': 'Bearer ' + token
-            }
-        },
-        (r) => {
-            if (200 <= r.statusCode && r.statusCode < 300) {
-                n_hit += 1;
-                response.writeHead(200);
-                r.pipe(response);
-            } else {
-                n_miss += 1;
-                response.writeHead(404);
-                response.end();
-            }
-        });
-    } else {
-        n_req += 1;
-        n_miss += 1;
-        response.writeHead(404);
-        response.end();
-    }
-});
+    const baseUrl = process.env.ACTIONS_CACHE_URL + '/_apis/artifactcache/';
+    const token = process.env.ACTIONS_RUNTIME_TOKEN;
 
-server.on('close', () => {
-    console.log("Goodbye! %d / %d == %f%%", n_hit, n_req, n_hit * 100 / (n_req? n_req: 1))
-});
+    server.on('request', (request, response) => {
+        const url = parse(request.url);
+        console.log(url.pathname);
+        if (url.pathname == '/close') {
+            server.close();
+            response.writeHead(200);
+            response.end(`Stats: ${ n_hit } / ${ n_req } == ${ n_hit * 100 / (n_req? n_req: 1) }%`);
+        } else if (url.pathname.startsWith('/cas/')) {
+            n_req += 1;
+            const hash = url.pathname.substring(5);
+            http.get(baseUrl + 'cache?key=cas-' + hash, {
+                headers: {
+                    'Authorization': 'Bearer ' + token
+                }
+            },
+            (r) => {
+                if (200 <= r.statusCode && r.statusCode < 300) {
+                    n_hit += 1;
+                    response.writeHead(200);
+                    r.pipe(response);
+                } else {
+                    n_miss += 1;
+                    response.writeHead(404);
+                    response.end();
+                }
+            });
+        } else {
+            n_req += 1;
+            n_miss += 1;
+            response.writeHead(404);
+            response.end();
+        }
+    });
 
-server.listen(3055);
+    server.on('close', () => {
+        console.log("Goodbye! %d / %d == %f%%", n_hit, n_req, n_hit * 100 / (n_req? n_req: 1))
+    });
+
+    server.listen(3055);
+}
